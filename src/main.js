f1ea1feca1c7d85773ad08b04ebfdff9ab6dfb60
村瀬勇磨 <y.murase@filiscreation.com>
2026-01-25 23:09:03 +0900

    モバイル全画面対応：レスポンシブデザイン改善

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import Phaser from 'phaser';
-import PrologueScene from './scenes/PrologueScene.js';
+import PrologueScene from './scenes/PrologueScene';
 
 const config = {
-    type: Phaser.AUTO,
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  parent: 'game-container',
+  backgroundColor: '#000000',
+  scene: [PrologueScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 1280,
-    height: 720,
-    parent: 'game-container',
-    backgroundColor: '#000000',
-    scene: [PrologueScene],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    }
+    height: 720
+  },
+  dom: {
+    createContainer: true
+  }
 };
 
 const game = new Phaser.Game(config);
